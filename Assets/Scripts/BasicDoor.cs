bfbc13c72b9cf2a07e83788bc6d48c035cf450c0
Nathan <nathmon565@gmail.com>
2022-02-14 22:05:56 -0700

    Add basic door
    Import QuickOutline, and set up a door in the scene.

---
+++ Assets/Scripts/BasicDoor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicDoor : MonoBehaviour {
+	public bool isOpen = true;
+	public GameObject mainDoor;
+	public GameObject otherDoor;
+	public Vector3 targetPos = new Vector3(1, 0, 0);
+	public float doorSpeed = 1;
+	private void Update() {
+		Vector3 p = isOpen ? targetPos : new Vector3();
+		mainDoor.transform.localPosition = Vector3.MoveTowards(mainDoor.transform.localPosition, p, doorSpeed * Time.deltaTime);
+		otherDoor.transform.localPosition = Vector3.MoveTowards(otherDoor.transform.localPosition, -p, doorSpeed * Time.deltaTime);
+	}
+	public void ToggleState() {
+		isOpen = !isOpen;
+	}
+}
